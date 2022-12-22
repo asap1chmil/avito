c14934a46975c6d3c3474628b3892307dbf54b71
HarnBark <barkqaqa@gmail.com>
2022-12-22 19:40:06 +0300

    feat: all photo seeds ready

--- db/seeders/20221222153326-photo.js
+++ db/seeders/20221222153326-photo.js
@@ -4,7 +4,37 @@ module.exports = {
     await queryInterface.bulkInsert('photos', [
       {
         house_id: '1',
-        path: '/img',
+        path: '/img/1.2.jpeg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        house_id: '1',
+        path: '/img/1.2.jpeg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        house_id: '1',
+        path: '/img/1.3.jpeg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        house_id: '2',
+        path: '/img/2.1.jpeg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        house_id: '2',
+        path: '/img/2.2.jpeg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        house_id: '3',
+        path: '/img/3.1.jpeg',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
