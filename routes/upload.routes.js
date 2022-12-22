15d5deba58a550358488b587bb385fb461a0546d
HarnBark <barkqaqa@gmail.com>
2022-12-22 17:59:45 +0300

    chore: router improve

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
+const fileMiddleware = require('../middlewares/upload');
 
 const router = Router();
 
-router.post('/upload', () => {
+router.post('/upload', fileMiddleware.single('House Photo'), (req, res) => {
   try {
+    if (req.file) {
+      res.json(req.file);
+    }
   } catch (error) {
     console.log(error);
   }
